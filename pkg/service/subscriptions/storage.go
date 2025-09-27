e382a5602425b30256a83b7644215e46ef6aeb02
lenkton <lenktongm@gmail.com>
2025-09-28 00:10:59 +0300

    feat: implement some basic Storage

---
+++ pkg/service/subscriptions/storage.go
@@ -0,0 +1,52 @@
+package subscriptions
+
+import (
+	"errors"
+	"slices"
+)
+
+type Storage struct {
+	subscriptions []*Subscription
+	lastID        int
+}
+
+var ErrSubscriptionNotFound = errors.New("subscription not found")
+
+func NewStorage() *Storage {
+	return &Storage{subscriptions: make([]*Subscription, 0)}
+}
+
+func (s *Storage) List() []*Subscription {
+	return s.subscriptions
+}
+
+func (s *Storage) Get(id int) (*Subscription, error) {
+	for _, si := range s.subscriptions {
+		if si.ID == id {
+			return si, nil
+		}
+	}
+	return nil, ErrSubscriptionNotFound
+}
+
+func (s *Storage) Add(sub *Subscription) int {
+	s.lastID++
+	sub.ID = s.lastID
+	s.subscriptions = append(s.subscriptions, sub)
+	return sub.ID
+}
+
+func (s *Storage) Update(sub *Subscription) error {
+	for _, si := range s.subscriptions {
+		if si.ID == sub.ID {
+			*si = *sub
+			return nil
+		}
+	}
+	return ErrSubscriptionNotFound
+}
+
+func (s *Storage) Delete(id int) {
+	s.subscriptions = slices.DeleteFunc(s.subscriptions,
+		func(si *Subscription) bool { return si.ID == id })
+}
